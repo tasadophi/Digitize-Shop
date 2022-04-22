eeda5be26f091996913a659681b602a12efb16f4
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-04-22 22:50:28 +0430

    semicolon removed

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <AppWrapper>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </AppWrapper>
   );
 }
