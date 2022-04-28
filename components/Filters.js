8edc1ae0acbeb620c9302e622781a6dcd7d969b0
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-04-28 20:01:30 +0430

    filter title added

--- components/Filters.js
+++ components/Filters.js
@@ -6,63 +6,68 @@ import CheckBox from "./CheckBox";
 
 const Filters = () => {
   return (
-    <ul className="flex flex-col gap-4 select-none">
-      <li>
-        <div className="flex items-center gap-8 cursor-pointer">
-          <div className="flex gap-2">
-            <span className="w-6 h-6">
-              <BrandIcon />
+    <>
+      <span className="text-orange-600 font-bold text-xl block my-6">
+        فیلتر
+      </span>
+      <ul className="flex flex-col gap-4 select-none">
+        <li>
+          <div className="flex items-center gap-8 cursor-pointer">
+            <div className="flex items-center gap-2">
+              <span className="w-6 h-6">
+                <BrandIcon />
+              </span>
+              برند محصول
+            </div>
+            <span>
+              <ChevronIcon />
             </span>
-            برند محصول
           </div>
-          <span>
-            <ChevronIcon />
-          </span>
-        </div>
-        <ul className="pt-4 flex flex-col gap-3">
-          <li>
-            <CheckBox label="اپل" id="apple" />
-          </li>
-          <li>
-            <CheckBox label="سامسونگ" id="samsung" />
-          </li>
-          <li>
-            <CheckBox label="شیائومی" id="xiaomi" />
-          </li>
-          <li>
-            <CheckBox label="هواوی" id="huawei" />
-          </li>
-        </ul>
-      </li>
-      <li>
-        <div className="flex items-center gap-8 cursor-pointer">
-          <div className="flex gap-2">
+          <ul className="pt-4 flex flex-col gap-3">
+            <li>
+              <CheckBox label="اپل" id="apple" />
+            </li>
+            <li>
+              <CheckBox label="سامسونگ" id="samsung" />
+            </li>
+            <li>
+              <CheckBox label="شیائومی" id="xiaomi" />
+            </li>
+            <li>
+              <CheckBox label="هواوی" id="huawei" />
+            </li>
+          </ul>
+        </li>
+        <li>
+          <div className="flex items-center gap-8 cursor-pointer">
+            <div className="flex items-center gap-2">
+              <span className="w-6 h-6">
+                <ColorIcon />
+              </span>
+              رنگ محصول
+            </div>
             <span className="w-6 h-6">
-              <ColorIcon />
+              <ChevronIcon />
             </span>
-            رنگ محصول
           </div>
-          <span className="w-6 h-6">
-            <ChevronIcon />
-          </span>
-        </div>
-        <ul></ul>
-      </li>
-      <li>
-        <div className="flex items-center gap-8 cursor-pointer">
-          <div className="flex gap-2">
+          <ul></ul>
+        </li>
+        <li>
+          <div className="flex items-center gap-8 cursor-pointer">
+            <div className="flex items-center gap-2">
+              <span className="w-6 h-6">
+                <PriceIcon />
+              </span>
+              محدوده قیمت
+            </div>
             <span className="w-6 h-6">
-              <PriceIcon />
+              <ChevronIcon />
             </span>
-            محدوده قیمت
           </div>
-          <span className="w-6 h-6">
-            <ChevronIcon />
-          </span>
-        </div>
-        <ul></ul>
-      </li>
-    </ul>
+          <ul></ul>
+        </li>
+      </ul>
+    </>
   );
 };
 
