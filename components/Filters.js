79d13ec9e214c8a923d853a069e7997556049c62
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-05-03 23:59:18 +0430

    accordions added and add getBrands and getColors too

--- components/Filters.js
+++ components/Filters.js
@@ -3,8 +3,43 @@ import ChevronIcon from "./icons/ChevronIcon";
 import ColorIcon from "./icons/ColorIcon";
 import PriceIcon from "./icons/PriceIcon";
 import CheckBox from "./CheckBox";
+import { useState } from "react";
+import { useProducts } from "../context/state";
+
+const getBrands = (products) => {
+  const allBrands = products.map((product) => product.brand);
+  return allBrands.filter((brand, index, arrey) => {
+    return (
+      index === arrey.findIndex((value) => value.brandEn === brand.brandEn)
+    );
+  });
+};
+
+const getColors = (products) => {
+  const allColors = [];
+  products.forEach((product) => {
+    product.colors.forEach((color) => allColors.push(color));
+  });
+  return allColors.filter((color, index, arrey) => {
+    return index === arrey.findIndex((value) => value.nameEn === color.nameEn);
+  });
+};
 
 const Filters = () => {
+  const [showMenu, setShowMenu] = useState({
+    brand: false,
+    color: false,
+    price: false,
+  });
+  const products = useProducts();
+  const brands = getBrands(products.allProducts);
+  const colors = getColors(products.allProducts);
+
+  // handler
+  const showHideMenu = (property) => {
+    setShowMenu({ ...showMenu, [property]: !showMenu[property] });
+  };
+
   return (
     <>
       <span className="text-orange-600 font-bold text-xl block my-6">
@@ -13,58 +48,92 @@ const Filters = () => {
       <ul className="flex flex-col gap-4 select-none">
         <li>
           <div className="flex items-center gap-8 cursor-pointer">
-            <div className="flex items-center gap-2">
+            <div
+              className="flex items-center gap-2"
+              onClick={() => showHideMenu("brand")}
+            >
               <span className="w-6 h-6">
                 <BrandIcon />
               </span>
               برند محصول
             </div>
-            <span>
+            <span
+              className={`w-6 h-6 ${showMenu["brand"] ? "rotate-180" : ""}`}
+            >
               <ChevronIcon />
             </span>
           </div>
-          <ul className="pt-4 flex flex-col gap-3">
-            <li>
-              <CheckBox label="اپل" id="apple" />
-            </li>
-            <li>
-              <CheckBox label="سامسونگ" id="samsung" />
-            </li>
-            <li>
-              <CheckBox label="شیائومی" id="xiaomi" />
-            </li>
-            <li>
-              <CheckBox label="هواوی" id="huawei" />
-            </li>
+          <ul
+            className={`pt-4 flex-col gap-3 ${
+              showMenu["brand"] ? "flex" : "hidden"
+            }`}
+          >
+            {brands.map((brand) => (
+              <CheckBox
+                key={brand.brandEn}
+                label={brand.brandFa}
+                id={brand.brandEn}
+              />
+            ))}
           </ul>
         </li>
         <li>
           <div className="flex items-center gap-8 cursor-pointer">
-            <div className="flex items-center gap-2">
+            <div
+              className="flex items-center gap-2"
+              onClick={() => showHideMenu("color")}
+            >
               <span className="w-6 h-6">
                 <ColorIcon />
               </span>
               رنگ محصول
             </div>
-            <span className="w-6 h-6">
+            <span
+              className={`w-6 h-6 ${showMenu["color"] ? "rotate-180" : ""}`}
+            >
               <ChevronIcon />
             </span>
           </div>
-          <ul></ul>
+          <ul
+            className={`pt-4 flex-col gap-3 ${
+              showMenu["color"] ? "flex" : "hidden"
+            }`}
+          >
+            {colors.map((color) => (
+              <CheckBox
+                key={color.nameEn}
+                label={color.nameFa}
+                id={color.nameEn}
+              />
+            ))}
+          </ul>
         </li>
         <li>
           <div className="flex items-center gap-8 cursor-pointer">
-            <div className="flex items-center gap-2">
+            <div
+              className="flex items-center gap-2"
+              onClick={() => showHideMenu("price")}
+            >
               <span className="w-6 h-6">
                 <PriceIcon />
               </span>
               محدوده قیمت
             </div>
-            <span className="w-6 h-6">
+            <span
+              className={`w-6 h-6 ${showMenu["price"] ? "rotate-180" : ""}`}
+            >
               <ChevronIcon />
             </span>
           </div>
-          <ul></ul>
+          <ul
+            className={`pt-4 flex-col gap-3 ${
+              showMenu["price"] ? "flex" : "hidden"
+            }`}
+          >
+            <li>
+              <input defaultValue="10" type="range" />
+            </li>
+          </ul>
         </li>
       </ul>
     </>
