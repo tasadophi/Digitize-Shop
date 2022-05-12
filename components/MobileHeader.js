2904758ea324dd4ee194f531e520cb715bfd88c6
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-05-12 17:30:11 +0430

    mobile header created

---
+++ components/MobileHeader.js
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import SearchIcon from "./icons/SearchIcon";
+import ChevronIcon from "./icons/ChevronIcon";
+
+const MobileHeader = ({ logo, title }) => {
+  return (
+    <div className="flex justify-between items-center py-6">
+      {logo ? (
+        <Image
+          src="/images/Logo.png"
+          alt="digitize logo"
+          width={50}
+          height={42}
+        />
+      ) : (
+        <span className="w-8 h-8 bg-white shadow-md rounded-xl p-1 flex justify-center items-center -rotate-90">
+          <ChevronIcon />
+        </span>
+      )}
+      <span className="text-slate-800 font-bold">{title}</span>
+      <span className="w-8 h-8 p-1 bg-white shadow flex justify-center items-center rounded">
+        <SearchIcon />
+      </span>
+    </div>
+  );
+};
+
+export default MobileHeader;
