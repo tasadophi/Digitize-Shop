33c3e1abacc5c49a64aebee4edfdf5bd3e2f8db2
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-05-12 18:21:51 +0430

    some icons editted

--- components/icons/CheckIcon.js
+++ components/icons/CheckIcon.js
@@ -2,7 +2,6 @@ const CheckIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
