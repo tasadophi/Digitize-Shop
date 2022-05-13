a9a7f404638a31dcb2de51b4c61c5c62eb510bca
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-05-13 23:36:52 +0430

    fix filters checkbox problem (same state and doesnt work correctly)

--- components/Products.js
+++ components/Products.js
@@ -46,27 +46,28 @@ const Products = ({ category }) => {
               </span>
               <span>فیلتر</span>
             </div>
-            {/* filter section modal */}
-            <div
-              className={`fixed inset-0  w-screen h-screen bg-stone-100 bg-opacity-80 ${
-                showFilters ? "z-10" : "-z-10"
-              }`}
-              onClick={() => setShowFilters(false)}
-            >
-              <div
-                className={`fixed transition-all w-full h-2/3 overflow-auto z-20 bg-white right-0 p-4 ${
-                  showFilters ? "bottom-0" : "-bottom-full"
-                }`}
-                onClick={(e) => e.stopPropagation()}
-              >
-                <Filters mobile={true} />
-              </div>
-            </div>
           </div>
         </div>
-        <div className="hidden col-span-1 p-6 bg-white rounded-xl max-h-[650px] lg:block">
-          <Categories />
-          <Filters />
+        <div className="col-span-1 max-h-[600px] overflow-auto rounded-xl bg-white">
+          <div className="hidden px-6 pt-6 lg:block">
+            <Categories />
+          </div>
+          {/* <Filters /> */}
+          <div
+            className={`fixed inset-0 bg-stone-100 pr-6 bg-opacity-80 lg:static lg:w-fit lg:h-fit lg:bg-white lg:-z-10 ${
+              showFilters ? "z-20 w-screen h-screen" : "-z-10"
+            }`}
+            onClick={() => setShowFilters(false)}
+          >
+            <div
+              className={`bg-white fixed transition-all w-full h-2/3 overflow-auto p-4 z-20 right-0 lg:overflow-visible lg:p-0 lg:static ${
+                showFilters ? "bottom-0" : "-bottom-full"
+              }`}
+              onClick={(e) => e.stopPropagation()}
+            >
+              <Filters setShowFilters={setShowFilters} />
+            </div>
+          </div>
         </div>
         <div className="col-span-5 flex flex-col gap-8 lg:col-span-4 mb-16">
           <div className="hidden bg-white w-full p-1 gap-2 items-center rounded-md lg:flex">
