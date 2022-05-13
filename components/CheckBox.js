e8e10dd05079201b034beca782f6025a4e83e4ed
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-05-13 23:35:18 +0430

    fix checked change color

--- components/CheckBox.js
+++ components/CheckBox.js
@@ -1,9 +1,29 @@
+import { useState } from "react";
+
 const CheckBox = ({ label, id }) => {
+  const [checked, setChecked] = useState(false);
+
   return (
     <div className="w-fit">
-      <input id={id} type="checkbox" className="hidden" />
-      <label htmlFor={id} className="flex items-center gap-2 select-none cursor-pointer">
-        <span className="w-3 h-3 rounded-sm ring-2 ring-orange-600 ring-offset-1 block"></span>
+      <input
+        id={id}
+        type="checkbox"
+        onChange={() => {
+          setChecked(!checked);
+        }}
+        checked={checked}
+        value={checked}
+        className="hidden"
+      />
+      <label
+        htmlFor={id}
+        className="flex items-center gap-2 select-none cursor-pointer"
+      >
+        <span
+          className={`w-3 h-3 rounded-sm ring-2 ring-orange-600 ring-offset-1 block ${
+            checked ? "bg-orange-600" : ""
+          }`}
+        ></span>
         {label}
       </label>
     </div>
