22e5174801f4e74d081027b51e43d76e9e4116c7
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-05-14 20:50:38 +0430

    add icon

---
+++ components/icons/XIcon.js
@@ -0,0 +1,27 @@
+const XIcon = () => {
+  return (
+    <svg viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <line
+        x1="9.90039"
+        y1="0.897482"
+        x2="0.897805"
+        y2="9.90007"
+        stroke="currentColor"
+        strokeWidth="1.26923"
+        strokeLinecap="round"
+      />
+      <line
+        x1="0.634615"
+        y1="-0.634615"
+        x2="13.3662"
+        y2="-0.634615"
+        transform="matrix(0.707107 0.707107 0.707107 -0.707107 1.1001 0)"
+        stroke="currentColor"
+        strokeWidth="1.26923"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
+
+export default XIcon;
