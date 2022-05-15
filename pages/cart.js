c5e15af120da228677c670956b42c5d53d6d33ba
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-05-15 17:34:55 +0430

    get data from cart

--- pages/cart.js
+++ pages/cart.js
@@ -2,13 +2,16 @@ import MobileHeader from "../components/MobileHeader";
 import Image from "next/image";
 import XIcon from "../components/icons/XIcon";
 import BottomMenu from "../components/BottomMenu";
+import { useShop } from "../context/state";
+import { sepratePrice } from "../lib/api";
+import Layout from "../Layout/Layout";
 
-const CartItem = () => {
+const CartItem = ({ product }) => {
   return (
     <div className="w-full flex gap-2 bg-white rounded-xl">
       <div className="relative w-14 h-24 mr-4">
         <Image
-          src="/images/iphone4.png"
+          src={product.image}
           alt="iphone"
           layout="fill"
           objectFit="contain"
@@ -16,10 +19,10 @@ const CartItem = () => {
       </div>
       <div className="flex flex-col justify-evenly overflow-hidden text-sm sm:text-lg">
         <span className="text-ellipsis whitespace-nowrap overflow-hidden">
-          گوشی آیفون 13 پرومکس
+          {product.model}
         </span>
         <span className="text-ellipsis whitespace-nowrap overflow-hidden font-medium text-orange-600">
-          44.444.444 تومان
+          {product.price} تومان
         </span>
       </div>
       <div className="flex flex-col justify-self-end items-end  mr-auto justify-between p-2">
@@ -41,27 +44,34 @@ const CartItem = () => {
 };
 
 const Cart = () => {
+  const shop = useShop();
+  const total = shop.cart.reduce((acc, curr) => acc + curr.price, 0);
   return (
-    <section className="container p-6">
-      <MobileHeader logo={false} title="سبد خرید" />
-      <div className="flex flex-col justify-center gap-2">
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-      </div>
-      <div className="bg-white flex flex-col gap-7 w-full p-7 mt-8 rounded-xl">
-        <div className="flex justify-between">
-          <span className="font-medium text-xl text-slate-800">
-            مجموع قیمت:
-          </span>
-          <span className="font-medium text-orange-600">10.254.545 تومان</span>
+    <Layout>
+      <section className="container p-6">
+        <MobileHeader logo={false} title="سبد خرید" />
+        <div className="flex flex-col justify-center gap-2">
+          {shop.cart.map((product) => (
+            <CartItem key={product.id} product={product} />
+          ))}
         </div>
-        <span>کد تخفیف دارید؟</span>
-      </div>
-      <div className="bg-orange-600 mb-12 mt-32 text-2xl rounded-xl py-4 flex justify-center text-white">ادامه فرایند خرید</div>
-      <BottomMenu />
-    </section>
+        <div className="bg-white flex flex-col gap-7 w-full p-7 mt-8 rounded-xl">
+          <div className="flex justify-between">
+            <span className="font-medium text-xl text-slate-800">
+              مجموع قیمت:
+            </span>
+            <span className="font-medium text-orange-600">
+              {sepratePrice(total)} تومان
+            </span>
+          </div>
+          <span>کد تخفیف دارید؟</span>
+        </div>
+        <div className="bg-orange-600 mb-12 mt-32 text-2xl rounded-xl py-4 flex justify-center text-white">
+          ادامه فرایند خرید
+        </div>
+        <BottomMenu />
+      </section>
+    </Layout>
   );
 };
 
