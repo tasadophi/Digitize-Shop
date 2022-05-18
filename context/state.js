a52339cc6fac81a379c88f0b400c37d37df2db13
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-05-18 19:29:39 +0430

    cart actions added

--- context/state.js
+++ context/state.js
@@ -10,16 +10,52 @@ const getCartItems = () => {
   if (data) return JSON.parse(data);
   return [];
 };
+
 const setCartItems = (cartItems) =>
   localStorage.setItem("cart", JSON.stringify(cartItems));
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "setCartItems": {
-      return { ...state, cart: action.cart };
+      return action.cart.length ? { ...state, cart: action.cart } : state;
     }
+
     case "addToCart": {
-      return { ...state, cart: [...state.cart, action.product] };
+      return {
+        ...state,
+        cart: [...state.cart, { ...action.product, count: 1 }],
+      };
+    }
+    case "increase": {
+      const newCart = state.cart.map(({ ...item }) => {
+        if (item.id === action.id) item.count++;
+        return item;
+      });
+      return {
+        ...state,
+        cart: newCart,
+      };
+    }
+    case "decrease": {
+      const cartItem = state.cart.find((item) => item.id === action.id);
+      const newCart =
+        cartItem.count === 1
+          ? state.cart.filter((item) => item.id !== action.id)
+          : state.cart.map(({ ...item }) => {
+              if (item.id === action.id) item.count--;
+              return item;
+            });
+      return {
+        ...state,
+        cart: newCart,
+      };
+    }
+    case "remove": {
+      const newCart = state.cart.filter((item) => item.id !== action.id);
+      return {
+        ...state,
+        cart: newCart,
+      };
     }
   }
 };
@@ -33,12 +69,12 @@ const AppWrapper = ({ children }) => {
   const [shop, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    shop.cart.length && setCartItems(shop.cart);
-  }, [shop.cart]);
-
-  useEffect(() => {
     dispatch({ type: "setCartItems", cart: getCartItems() });
   }, []);
+
+  useEffect(() => {
+    setCartItems(shop.cart);
+  }, [shop.cart]);
 
   return (
     <Shop.Provider value={shop}>
