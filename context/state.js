12e8132eca175e7a630756232fb5de9a735cb326
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-05-23 20:38:26 +0430

    filter action and its dependencies added

--- context/state.js
+++ context/state.js
@@ -1,6 +1,7 @@
 import React, { useReducer, useContext } from "react";
 import { useEffect } from "react";
 const jsonData = require("./../data.json");
+import { useRouter } from "next/router";
 
 const Shop = React.createContext();
 const ShopDispatcher = React.createContext();
@@ -16,6 +17,7 @@ const setCartItems = (cartItems) =>
 
 const reducer = (state, action) => {
   switch (action.type) {
+    // cart actions
     case "setCartItems": {
       return action.cart.length ? { ...state, cart: action.cart } : state;
     }
@@ -57,16 +59,96 @@ const reducer = (state, action) => {
         cart: newCart,
       };
     }
+    // filter actions
+    case "filter": {
+      const category = action.category;
+      const filtersObject = action.filtersObject;
+      const filterKeys = Object.keys(filtersObject);
+      const products = [];
+      if (Object.keys(filterKeys).length) {
+        state.allProducts.forEach((product) => {
+          const productFilters = [];
+          filterKeys.forEach((fKey) => {
+            if (fKey === "brand") {
+              if (filtersObject[fKey].includes(product.brand.brandEn)) {
+                if (category) {
+                  if (product.category === category)
+                    !productFilters.includes("brandOk") &&
+                      productFilters.push("brandOk");
+                } else
+                  !productFilters.includes("brandOk") &&
+                    productFilters.push("brandOk");
+              }
+            } else if (fKey === "colors") {
+              product.colors.forEach((colorObject) => {
+                if (filtersObject[fKey].includes(colorObject.nameEn)) {
+                  if (category) {
+                    if (product.category === category)
+                      !productFilters.includes("colorOk") &&
+                        productFilters.push("colorOk");
+                  } else
+                    !productFilters.includes("colorOk") &&
+                      productFilters.push("colorOk");
+                }
+              });
+            } else if (fKey === "price") {
+              if (product.price <= parseInt(filtersObject[fKey])) {
+                if (category) {
+                  if (product.category === category)
+                    !productFilters.includes("priceOk") &&
+                      productFilters.push("priceOk");
+                } else
+                  !productFilters.includes("priceOk") &&
+                    productFilters.push("priceOk");
+              }
+            }
+          });
+          if (productFilters.length === Object.keys(filtersObject).length)
+            products.push(product);
+        });
+        return { ...state, products: products };
+      } else return { ...state, products: state.allProducts };
+    }
   }
 };
 
 const initialState = {
   allProducts: jsonData,
+  products: jsonData,
   cart: [],
+};
+
+const addFilter = (key, splitedArrey, allFilters) => {
+  if (Object.keys(allFilters).includes(key)) {
+    allFilters[key].push(splitedArrey[1]);
+  } else {
+    allFilters[key] = [splitedArrey[1]];
+  }
 };
 
 const AppWrapper = ({ children }) => {
   const [shop, dispatch] = useReducer(reducer, initialState);
+  const router = useRouter();
+  useEffect(() => {
+    const allFilters = {};
+    const allkeys = Object.keys(router.query);
+    allkeys.forEach((key) => {
+      const brand = key.split("brand");
+      const colors = key.split("colors");
+      if (brand.length > 1) {
+        addFilter("brand", brand, allFilters);
+      } else if (colors.length > 1) {
+        addFilter("colors", colors, allFilters);
+      } else if (key === "price") {
+        allFilters["price"] = router.query["price"];
+      }
+    });
+    dispatch({
+      type: "filter",
+      filtersObject: allFilters,
+      category: router.query["category"] ? router.query["category"] : false,
+    });
+  }, [router.query]);
 
   useEffect(() => {
     dispatch({ type: "setCartItems", cart: getCartItems() });
@@ -85,7 +167,7 @@ const AppWrapper = ({ children }) => {
   );
 };
 
-export default AppWrapper;
+export default React.memo(AppWrapper);
 
 export const useShop = () => useContext(Shop);
 export const useShopDispatcher = () => useContext(ShopDispatcher);
