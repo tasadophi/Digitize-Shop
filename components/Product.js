4b740063f18347303f1196b09a1604a81f55d66d
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-06-02 12:20:24 +0430

    fix ui on mobile

--- components/Product.js
+++ components/Product.js
@@ -21,7 +21,7 @@ const getColors = (colorCode) => {
 const Product = ({ product }) => {
   return (
     <>
-      <div className="bg-white p-2 rounded-xl flex flex-col gap-4">
+      <div className="bg-white p-2 rounded-xl flex flex-col gap-4 justify-between">
         <div className="bg-stone-200 rounded-lg flex justify-center items-center p-6">
           <Image
             src={product.image}
@@ -31,9 +31,15 @@ const Product = ({ product }) => {
                 ? 140
                 : product.category === "laptops"
                 ? 220
-                : 140
+                : 130
             }
-            height={165}
+            height={
+              product.category === "mobiles"
+                ? 165
+                : product.category === "laptops"
+                ? 165
+                : 160
+            }
           />
         </div>
         <div className="flex justify-between items-center">
@@ -42,7 +48,7 @@ const Product = ({ product }) => {
             {product.colors.map((color) => getColors(color.hex))}
           </div>
         </div>
-        <div className="flex flex-col text-sm gap-2 pb-2 border-b border-orange-200">
+        <div className="flex flex-col text-sm gap-2">
           <span className="whitespace-nowrap text-ellipsis overflow-hidden">
             {product.model}
           </span>
@@ -50,7 +56,7 @@ const Product = ({ product }) => {
             {sepratePrice(product.price)} تومان
           </span>
         </div>
-        <button className="text-orange-600 font-bold">
+        <button className="text-orange-600 font-bold  border-t border-orange-200 pt-2">
           <Link href={`/products/${product.category}/${product.id}`}>
             مشاهده و سفارش
           </Link>
